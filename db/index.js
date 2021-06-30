0db6a4d9efa26fb6ee231a8aa8a2324ad8bb7738
Chris McClelland <clm727@gmail.com>
2021-06-30 17:37:56 -0400

    changed ip

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'chris',
-  host: 'localhost',
+  host: '18.116.61.39',
   database: 'qanda',
   password: 'password',
   port: 5432,
